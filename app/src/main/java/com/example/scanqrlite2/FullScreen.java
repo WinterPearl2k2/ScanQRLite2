a26fac626e3935383d707d9d76b7125b1877c391
nhuquynh2603 <nnhuquynh2603@gmail.com>
2022-07-13 15:47:35 +0700

    #ref ui setting

--- app/src/main/java/com/example/scanqrlite2/FullScreen.java
+++ app/src/main/java/com/example/scanqrlite2/FullScreen.java
@@ -24,7 +24,7 @@ public class FullScreen {
             activity.getWindow().getDecorView().setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                     | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-            activity.getWindow().setStatusBarColor(activity.getResources().getColor(R.color.secondaryShrinePink));
+            activity.getWindow().setStatusBarColor(activity.getResources().getColor(R.color.primaryShrinePink));
         }
     }
 }
