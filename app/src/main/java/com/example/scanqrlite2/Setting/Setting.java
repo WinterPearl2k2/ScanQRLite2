1e2c3f3b30d8023f5e7d40a54e283efcb0295234
nhuquynh2603 <nnhuquynh2603@gmail.com>
2022-07-18 10:33:59 +0700

    fix ui history

--- app/src/main/java/com/example/scanqrlite2/Setting/Setting.java
+++ app/src/main/java/com/example/scanqrlite2/Setting/Setting.java
@@ -63,7 +63,6 @@ public class Setting extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_setting, container, false);
         ORM(view);
         Beep();
